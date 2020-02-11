630d7d5d243db64c4e974e5f3aef5889ca36eb67
Jarl H <jarlah@pm.me>
2020-02-11 19:23:11 +0100

    dont need type declaration for everything

--- app/Main.hs
+++ app/Main.hs
@@ -4,93 +4,57 @@ import Lib
 import Data.Char
 import Text.Read (readMaybe, readEither)
 import Data.List (intercalate)
-
-winningValue :: Int
-winningValue = 21
-
-winFactor :: Double
-winFactor = 1.5
-
-shuffleTwice :: [Card] -> IO [Card]
-shuffleTwice cards = shuffle cards >>= shuffle
+import System.Random (StdGen)
 
 data Suit = Heart | Diamond | Spade | Club deriving(Eq, Show, Enum)
-
 data Rank = Ace | Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King deriving(Enum, Eq, Show, Bounded)
+type Card = (Suit, Rank)
+type Deck = [Card]
+type Hand = [Card]
 
-rankValue :: Rank -> Int
+winningValue = 21
+winFactor = 1.5
+
 rankValue rank | rank == King || rank == Queen || rank == Jack = 10
 rankValue rank = fromEnum rank + 1
 
-data Card = Card Suit Rank deriving(Show)
+cards = [(suit, rank) | suit <- [Heart ..], rank <- [Ace ..]]
 
-cards :: [Card]
-cards = [Card suit rank | suit <- [Heart ..], rank <- [Ace ..]]
+handValue [(_, rank)] = rankValue rank
+handValue ((_,rank):xs) = rankValue rank + handValue xs
 
-newtype Deck = Deck [Card] deriving(Show)
+containsAce [(_, rank)] = Ace == rank
+containsAce ((_,rank):xs) = Ace == rank || containsAce xs
 
-deal :: Deck -> (Card, Deck)
-deal (Deck (topCard:remainingCards)) =
-  (topCard, Deck remainingCards)
+specialHandValue hand = if containsAce hand then handValue hand + 10 else handValue hand
 
-newtype Hand = Hand [Card] deriving(Show)
-
-handValue :: Hand -> Int
-handValue (Hand [Card suit rank]) = rankValue rank
-handValue (Hand (Card suit rank:remainingCards)) = rankValue rank + handValue (Hand remainingCards)
-
-containsAce :: Hand -> Bool
-containsAce (Hand [Card suit rank]) = Ace == rank
-containsAce (Hand (Card suite rank:xs)) = Ace == rank || containsAce (Hand xs)
-
-specialHandValue :: Hand -> Int
-specialHandValue hand =
-  if containsAce hand
-    then value + 10
-    else value
-  where value = handValue hand
-
-isBlackJack :: Hand -> Bool
-isBlackJack hand = handValue hand == winningValue || specialHandValue hand == winningValue
-
-isBust :: Hand -> Bool
 isBust hand = handValue hand > winningValue
 
-bestValue :: Hand -> Int
-bestValue hand =
-  if value <= winningValue
-    then value
-    else 0
-  where value = max (handValue hand) (specialHandValue hand)
+bestValue hand = if value <= winningValue then value else 0 where value = max (handValue hand) (specialHandValue hand)
 
-winsOver :: Hand -> Hand -> Bool
 winsOver this that = bestValue this > bestValue that
 
 showCards :: Hand -> Bool -> String
-showCards (Hand (Card suit rank:xs)) dealer
-  | dealer = show rank ++ ", X"
-  | not (null xs) = show rank ++ ", " ++ showCards (Hand xs) dealer
-  | otherwise = show rank
-
-addCard :: Hand -> Card -> Hand
-addCard (Hand cards) card = Hand (card : cards)
+showCards (card:xs) dealer
+  | dealer = show card ++ ", X"
+  | not (null xs) = show card ++ ", " ++ showCards xs dealer
+  | otherwise = show card
 
 dealHands :: Deck -> (Hand, Hand, Deck)
 dealHands deck = (hand1, hand2, newDeck)
   where
-    (firstCard, deck1) = deal deck
-    (secondCard, deck2) = deal deck1
-    (thirdCard, deck3) = deal deck2
-    (fourthCard, newDeck) = deal deck3
-    hand1 = Hand [firstCard, secondCard]
-    hand2 = Hand [thirdCard, fourthCard]
+    (firstCard, deck1) = (head deck, tail deck)
+    (secondCard, deck2) = (head deck1, tail deck1)
+    (thirdCard, deck3) = (head deck2, tail deck2)
+    (fourthCard, newDeck) = (head deck3, tail deck3)
+    hand1 = [firstCard, secondCard]
+    hand2 = [thirdCard, fourthCard]
 
 dealerTryToWin :: Hand -> Deck -> (Hand, Deck)
 dealerTryToWin hand deck | handValue hand < 17 =
-  dealerTryToWin (addCard hand card) newDeck
-  where (card, newDeck) = deal deck
-dealerTryToWin hand deck =
-  (hand, deck)
+  dealerTryToWin (card : hand) newDeck
+  where (card, newDeck) = (head deck, tail deck)
+dealerTryToWin hand deck = (hand, deck)
 
 type RoundData = (Hand, Hand, Deck, Bool)
 
@@ -98,9 +62,8 @@ hitOrStand :: Hand -> Hand -> Deck -> Bool -> RoundData
 hitOrStand playerHand dealerHand deck hit =
   if hit
     then -- lets hit
-      let (card, newDeck) = deal deck
-          newPlayerHand = addCard playerHand card
-      in (newPlayerHand, dealerHand, newDeck, False)
+      let (card, newDeck) = (head deck, tail deck)
+      in (card : playerHand, dealerHand, newDeck, False)
     else -- let the dealer try to win
       let (newDealerHand, newDeck) = dealerTryToWin dealerHand deck
       in (playerHand, newDealerHand, newDeck, True)
@@ -146,14 +109,9 @@ doRoundLoop (playerHand, dealerHand, deck, stand)
   | stand = doShowSummary playerHand dealerHand (isBust dealerHand || winsOver playerHand dealerHand)
   | otherwise = doHitOrStand playerHand dealerHand deck >>= doRoundLoop
 
--- deal hands with shuffled deck.
---  do the round loop with the dealt hands and new deck
---    If money left
---      ask if user want to continue.
---      Otherwise quit.
 doGameLoop :: Int -> Int -> IO Int
 doGameLoop bet currentCredit =
-  dealHands . Deck <$> shuffleTwice cards >>= \(playerHand, dealerHand, newDeck) ->
+  dealHands <$> (shuffle cards >>= shuffle) >>= \(playerHand, dealerHand, newDeck) ->
     doRoundLoop (playerHand, dealerHand, newDeck, False) >>= \playerWon ->
       let newCredit =
             if playerWon
